7c8bae67032486ae7af3a38c1242473cceb8a8d4
David Silva <david.silva@lti.net.br>
2023-05-17 18:23:54 -0300

    Validações de update e show no Marca e iniciando o Model de Modelos

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    protected $marca;
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,8 +19,8 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        $marcas = Marca::all();
-        return $marcas;
+        $marcas = $this->marca->all();
+        return response()->json($marcas, 200);
     }
 
     /**
@@ -36,41 +41,80 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
         $marca = Marca::create($request->all());
-        return $marca;
+        return response()->json($marca, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        //
-    }
+        $marca = $this->marca->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Marca $marca)
-    {
-        //
+        if (is_null($marca)) {
+            return response()->json(['erro' => "Marca pesquisada não existe."], 404);
+        }
+
+        return response()->json($marca, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $marca = $this->marca->find($id);
+
+        if (is_null($marca)) {
+            return response()->json(['erro' => "Impossível atualizar o registro. A marca não existe!"], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+             
+            // Percorrendo regras definidas no Model
+            foreach ($marca->rules() as $input => $regra) {
+
+                // Pegando apenas regras que se encaixem neste contexto
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas, $marca->feedback());
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+        $marca->update($request->all());
+
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
      * @param  \App\Models\Marca  $marca
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function destroy($id)
     {
-        //
+        $marca = $this->marca->find($id);
+
+        if (is_null($marca)) {
+            return response()->json(['erro' => "Impossível deletar o registro. A marca selecionada não existe!"], 404);
+        }
+
+        $marca->delete();
+        return response()->json(['msg' => "Marca Removida"], 200);   
     }
 }
--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['marca_id', 'nome', 'imagem', 'numero_portas', 'lugares'];
+
+    public function rules()
+    {
+        return [
+            'nome' => 'required|unique:marcas,nome,'. $this->id.'|min:3', 
+            'imagem' => 'required'
+        ];
+    }
 }
